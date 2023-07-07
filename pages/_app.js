b323a014933c3d5ca931fcd6aaee4cf9eee40615
Dev-Yong <dragonyongs@naver.com>
2023-07-07 18:48:07 +0900

    tailwindCSS 설정 테스트

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import Layout from '../components/layout'
+import '../styles/tailwind.css';
  
 export default function MyApp({ Component, pageProps }) {
   return (
--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,13 @@
+import '../styles/tailwind.css';
+
 export default function About() {
     return (
+        <>
         <h1>About</h1>
+        <div className="text-3xl font-bold underline">
+            Hello, world!
+        </div>
+        </>
     )
 }
+  
